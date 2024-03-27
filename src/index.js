ce90049590b4e91e9664ffb149d5ce03fd5ca268
HimaniSatija <HimaniSatija98@gmail.com>
2024-03-27 16:24:36 +0530

    final changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/little-lemon">
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
